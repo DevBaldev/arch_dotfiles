b0b1485df21bdcbe80d926a16c8992547ffe82fd
DevBaldev <triggerflax@gmail.com>
2025-07-23 15:57:10 +0530

    fix formate

--- install/install.py
+++ install/install.py
@@ -1,8 +1,11 @@
+"""Simple installations script."""
+
 import os
-import tomllib
+import toml
 
 
 def main() -> None:
+    """Call all the relevant funtions."""
     sys_update()
     paru_install()
     pkgs_install()
@@ -12,26 +15,27 @@ def main() -> None:
 
 def sys_update() -> None:
     """Update whole system."""
-    os.system(f"sudo pacman -Syu")
+    os.system("sudo pacman -Syu")
 
 
 def paru_install() -> None:
     """Installing aur helper (paru)."""
-    os.system("git clone https://aur.archlinux.org/paru.git")  # Clone paru repo
+    os.system(
+        "git clone https://aur.archlinux.org/paru.git")  # Clone paru repo
     os.system("cd paru; makepkg -si")  # Makeing the pkg
     os.system("sudo rm -r paru")  # removing paru repo
 
 
 def load_toml() -> dict:
     """Load TOML data from applications.toml"""
-    with open("applications.toml", "rb") as f:
-        toml_data: dict = tomllib.load(f)
+    with open("applications.toml", "r") as f:
+        toml_data: dict = toml.load(f)
         return toml_data
 
 
 def pkgs_install() -> None:
     """Install list of applications."""
-    pkgs: list = [i for i in load_toml().values()]
+    pkgs: list = list(load_toml().values())
     os.system(f"paru -Sy --needed {' '.join(pkgs)}")
 
 
