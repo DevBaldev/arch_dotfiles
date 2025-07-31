e39dce4cc1d55a604e1423335abec0af32f38113
DevBaldev <triggerflax@gmail.com>
2025-07-31 07:19:25 +0530

    31/07/25 07:19

--- install.sh
+++ install.sh
@@ -21,13 +21,16 @@ uninstall_pkgs() {
 pkgs_install() {
   install_pkgs mesa vulkan-radeon
   install_pkgs pipewire{,-{pulse,jack}} wireplumber
-  install_pkgs noto-fonts-{cjk,emoji,extra} ttf-{cascadia-code,jetbrains-mono}-nerd
+  install_pkgs noto-fonts-{cjk,emoji,extra}
+  install_pkgs ttf-{cascadia-code,jetbrains-mono}-nerd
   install_pkgs hypr{land,paper,polkitagent} waybar mako ghostty wofi
   install_pkgs qt{5,6} nwg-look kvantum{,-qt5} qt6ct
   uninstall_pkgs qt{5,6}-tools
-  install_pkgs rust uutils-coreutils lsd bat zoxide ripgrep dua-cli yazi ffmpeg 7zip jq poppler fd fzf imagemagick btop
+  install_pkgs rust uutils-coreutils lsd bat zoxide ripgrep dua-cli
+  install_pkgs yazi ffmpeg 7zip jq poppler fd fzf imagemagick btop
   install_pkgs imv mpv yt-dlp zathura-{cb,pdf-mupdf}
-  install_pkgs {{bash,lua}-language,python-lsp}-server tombi rust-analyzer stylua ruff shfmt
+  install_pkgs {{bash,lua}-language,python-lsp}-server rust-analyzer
+  install_pkgs stylua ruff shfmt tombi
   install_pkgs network-manager-applet blueman pavucontrol-qt bluez{,-tools}
   install_pkgs obsidian spotify-launcher
   install_pkgs wofi-emoji ani-cli {librewolf,freetube}-bin
